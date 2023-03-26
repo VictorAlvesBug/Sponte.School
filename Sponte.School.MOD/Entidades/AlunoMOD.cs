bb227fea2b975ee1e22f6d3f8e7000d558241c73
Victor Alves Bugueno <victor.bugueno@fiap.com.br>
2023-03-26 16:10:05 -0300

    Removido EntityMOD

--- Sponte.School.MOD/Entidades/AlunoMOD.cs
+++ Sponte.School.MOD/Entidades/AlunoMOD.cs
@@ -7,8 +7,26 @@ using System.IO;
 
 namespace Sponte.School.MOD.Entidades
 {
-    public class AlunoMOD : EntityMOD
+    public class AlunoMOD
 	{
+		[BsonId(IdGenerator = typeof(StringObjectIdGenerator))]
+		[BsonRepresentation(BsonType.ObjectId)]
+		public string Id { get; set; }
+
+		[BsonElement("nome")]
+		public string Nome { get; set; }
+
+		[BsonElement("cpf")]
+		public string CPF { get; set; }
+
+		[BsonElement("foto")]
+		public string FotoBase64 { get; set; }
+
+		[BsonElement("dataNascimento")]
+		public DateTime DataNascimento { get; set; }
+
+		[BsonElement("email")]
+		public string Email { get; set; }
 
 		[BsonElement("nome")]
 		public string Nome { get; set; }
